77762cbf1445f155b2942789b228526632e3f6e2
Sangjin Kim <rla2892@naver.com>
2024-10-07 07:56:54 +0900

    Make layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Link from "next/link"
+
+const Navbar = () => {
+    return (
+        <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+            <ul style={{ display: "flex", listStyle: "none", gap: "20px" }}>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/inventory">Inventory</Link>
+                </li>
+                <li>
+                    <Link href="/orders">Orders</Link>
+                </li>
+                <li>
+                    <Link href="/profile">Profile</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
